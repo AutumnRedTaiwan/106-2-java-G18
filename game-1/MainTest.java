3dd422b38afcec16ffd40ea5bf361375898df2ae
Dakudonkuro <39295051+Dakudonkuro@users.noreply.github.com>
2018-06-06 06:54:52 +0800

    Update MainTest.java

--- game-1/MainTest.java
+++ game-1/MainTest.java
@@ -1,13 +1,40 @@
 package game;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 
 public class MainTest {
+	
+	static	String Filename;
+	static	BufferedImage image;
 
 	public static void main(String[] args) {
+		
 		DrawMainFrame app = new DrawMainFrame();
-		app.setSize(800,600);
-		app.setVisible(true);
+
+		Filename="background.png";
+		try
+	    {
+	            image=ImageIO.read(new File(Filename));
+	    }
+	    catch(Exception e)
+	    {
+	            javax.swing.JOptionPane.showMessageDialog(null, "載入圖檔錯誤: "+Filename);
+	            image=null;
+	    }
+		JScrollPane scrollPane = new JScrollPane(new JLabel(new ImageIcon(image)));
+		
+		app.getContentPane().add(scrollPane);
+		app.pack();
+		app.setDefaultCloseOperation(DrawMainFrame.EXIT_ON_CLOSE);
+		app.setTitle(Filename+" "+image.getWidth()+" x "+image.getHeight());
 		app.setLocationRelativeTo(null);
+		app.setVisible(true);  
 	}
 
 }
